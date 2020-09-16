f9633011efb72f7eb0585948403a8059028b8077
Zztorp <Zztormen@hotmail.com>
2020-09-16 12:01:12 +0200

    commit all files

---
+++ script.js
@@ -0,0 +1,12 @@
+/*const url = "https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/Europe/Stockholm.json";
+
+const info = document.getElementById('clock');
+
+fetch(url)
+.then(clockResp => clockResp.json())
+.then(clockData =>
+{
+    info.innerHTML = `Klockan<br><br>${clockData.datetime.substring(11,16)}`;
+})
+.catch(error => console.error('Error with API call ', error));
+*/
