f30a3bd56a39f156d1806b295d03abe96ea1ac78
Zztorp <Zztormen@hotmail.com>
2020-10-21 19:39:08 +0200

    Last update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/*const url = "https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/Europe/Stockholm.json";
+/*
+const url = "https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/Europe/Stockholm.json";
 
 const info = document.getElementById('clock');
 
@@ -12,3 +13,32 @@ fetch(url)
 */
 
 
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const errorElement = document.getElementById('error');
+const submit = document.getElementById('submit');
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if(name.value === '' || name.value == null){
+        messages.push('Name is required')
+            //document.write("\n");
+            
+    }
+
+    if(password.value === ''|| password.value == null){
+        messages.push('Please fill in Password')
+    }
+
+    if(email.value === '' || email.value == null){
+        messages.push('Please fill in Email')
+    }
+
+    if(messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(',')
+    }
+        
+})
